8ff3d61448a3680e3aa646e2a06ecb68b8632138
Shiv Gupta <shivgupta@Shivs-MacBook-Pro.local>
2021-02-07 14:52:36 -0500

    Completed registration and login form

--- Flask_Blog/flaskblog.py
+++ Flask_Blog/flaskblog.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash,redirect
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'bc4358412d29bfabaeb83884e7b62777'
 
 
 posts = [
@@ -25,6 +28,29 @@ def home():
 @app.route("/about")
 def about():
     return render_template('about.html', title ='about')
+
+
+
+@app.route("/register", methods = ['GET','POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form = form)
+
+@app.route("/login", methods = ['GET','POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful.', 'danger')
+
+    return render_template('login.html', title='Login', form = form)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
