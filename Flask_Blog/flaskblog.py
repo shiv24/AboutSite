2b131a167c5fd38367d8b4e4b5a42e1d56f59f2a
Shiv Gupta <shivgupta@Shivs-MacBook-Pro.local>
2021-02-07 13:32:18 -0500

    Flask blog scaffolding

---
+++ Flask_Blog/flaskblog.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, url_for
+app = Flask(__name__)
+
+
+posts = [
+    {
+        'author': 'Corey Schafer',
+        'title': 'Blog Post 1',
+        'content': 'First Post conent',
+        'date_posted': 'April 20th 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second Post conent',
+        'date_posted': 'April 21st 2018'
+    }
+]
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html', posts = posts)
+
+@app.route("/about")
+def about():
+    return render_template('about.html', title ='about')
+
+if __name__ == "__main__":
+    app.run(debug=True)
